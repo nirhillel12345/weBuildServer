854ed8650f5df8669db067f930745f97bc68b96c
nirhillel12345 <nirhillel12345@gmail.com>
2023-11-18 00:13:45 +0200

    fixing class

--- src/main/java/com/example/webuildserver/controllers/projectsControllers.java
+++ src/main/java/com/example/webuildserver/controllers/projectsControllers.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "projects")
-public class ProjectsControllers {
+class ProjectsControllers {
     @Autowired
     private ProjectsService projectsService;
 
