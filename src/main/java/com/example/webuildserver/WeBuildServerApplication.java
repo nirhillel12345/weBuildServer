7e80d98eb473909ee23011c0528f1828d2647457
nirhillel12345 <nirhillel12345@gmail.com>
2023-11-19 21:22:37 +0200

    testing add images folders

--- src/main/java/com/example/webuildserver/WeBuildServerApplication.java
+++ src/main/java/com/example/webuildserver/WeBuildServerApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WeBuildServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("before running the spring server!!!!!");
         SpringApplication.run(WeBuildServerApplication.class, args);
     }
 
